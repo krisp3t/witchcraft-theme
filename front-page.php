4e935c1efd68033582b57313b0b101cf630f172b
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-06-27 18:39:38 +0200

    Static feature cards Hero button, max font size

--- front-page.php
+++ front-page.php
@@ -22,13 +22,15 @@ $container = get_theme_mod('understrap_container_type');
 
 
 <!-- Carousel -->
-<div id="carouselHeroImage" class="carousel slide" data-ride="carousel">
+<div id="carouselHeroImage" class="carousel slide shadow-sm" data-ride="carousel">
 	<div class="carousel-inner">
 		<div class="carousel-item active">
 			<div class="container d-flex h-100 align-items-center justify-content-center">
 				<div class="carousel-caption">
 					<h1 class="text-shadow-lg">Witchcraft Theme</h1>
 					<p class="text-shadow-lg">A theme for witches and wizards. Made for WordPress blogs and WooCommerce stores.</p>
+					<button type="button" class="btn btn-primary"> Visit our store
+					</button>
 				</div>
 			</div>
 		</div>
@@ -41,14 +43,37 @@ $container = get_theme_mod('understrap_container_type');
 
 		<div class="row">
 
-			<main class="site-main" id="main">
+			<main class="site-main container-fluid" id="main">
+				<div class="row">
+					<div class="col-sm card text-center mx-2 py-3 shadow-sm">
+						<i class="fa fa-shopping-cart fa-3x"></i>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+					<div class="col-sm card text-center mx-2 py-3 shadow-sm">
+						<i class="fa fa-users fa-3x"></i>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+					<div class="col-sm card text-center mx-2 py-3 shadow-sm">
+						<i class="fa fa-book fa-3x"></i>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+				</div>
+		</div>
 
+		</main><!-- #main -->
 
-			</main><!-- #main -->
+	</div><!-- .row -->
 
-		</div><!-- .row -->
-
-	</div><!-- #content -->
+</div><!-- #content -->
 
 </div><!-- #index-wrapper -->
 
