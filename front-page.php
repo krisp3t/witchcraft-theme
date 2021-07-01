4f18c7d62eaefa9c2b8b5d1533a536617c94bc8b
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-07-01 18:58:23 +0200

    Add front-page loop template

--- front-page.php
+++ front-page.php
@@ -51,7 +51,7 @@ $container = get_theme_mod('understrap_container_type');
 				<?php
 				while (have_posts()) {
 					the_post();
-					get_template_part('loop-templates/content', 'page');
+					get_template_part('loop-templates/content-front', 'page');
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if (comments_open() || get_comments_number()) {
