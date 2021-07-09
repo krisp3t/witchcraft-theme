3bbe90f89a5f1f85c3a43f9da09a3e915d726240
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-07-09 16:11:47 +0200

    Improve HTML escaping

--- template-parts/blocks/feature-cols.php
+++ template-parts/blocks/feature-cols.php
@@ -32,9 +32,9 @@ $column_3_icon = esc_attr(get_field('column_3_icon')) ?: '';
 $column_1_title = esc_html(get_field('column_1_title')) ?: '';
 $column_2_title = esc_html(get_field('column_2_title')) ?: '';
 $column_3_title = esc_html(get_field('column_3_title')) ?: '';
-$column_1_text = esc_html(get_field('column_1_text')) ?: '';
-$column_2_text = esc_html(get_field('column_2_text')) ?: '';
-$column_3_text = esc_html(get_field('column_3_text')) ?: '';
+$column_1_text = get_field('column_1_text') ?: ''; // Don't forget to escape!
+$column_2_text = get_field('column_2_text') ?: '';
+$column_3_text = get_field('column_3_text') ?: '';
 
 
 ?>
@@ -45,21 +45,21 @@ $column_3_text = esc_html(get_field('column_3_text')) ?: '';
 			<i class="<?php echo $column_1_icon; ?> fa-3x"></i>
 			<div class="card-body">
 				<h5 class="card-title"><?php echo $column_1_title ?></h5>
-				<p class="card-text"><?php echo $column_1_text ?></p>
+				<p class="card-text"><?php echo wp_kses($column_1_text, '') ?></p>
 			</div>
 		</div>
 		<div class="col-sm card text-center my-2 my-sm-0 mx-sm-2 py-3 shadow-sm border">
 			<i class="<?php echo $column_2_icon; ?> fa-3x"></i>
 			<div class="card-body">
 				<h5 class="card-title"><?php echo $column_2_title ?></h5>
-				<p class="card-text"><?php echo $column_2_text ?></p>
+				<p class="card-text"><?php echo wp_kses($column_2_text, '') ?></p>
 			</div>
 		</div>
 		<div class="col-sm card text-center my-2 my-sm-0 ml-sm-2 py-3 shadow-sm border">
 			<i class="<?php echo $column_3_icon; ?> fa-3x"></i>
 			<div class="card-body">
 				<h5 class="card-title"><?php echo $column_3_title ?></h5>
-				<p class="card-text"><?php echo $column_3_text ?></p>
+				<p class="card-text"><?php echo wp_kses($column_3_text, '') ?></p>
 			</div>
 		</div>
 	</div>
